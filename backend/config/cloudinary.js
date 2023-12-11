9af99b4ca0ff1a39d143ac6cdfdf4a24034cb21b
Altaf Alam <102013452+Altafalam3@users.noreply.github.com>
2023-12-11 19:05:24 +0530

    Update cloudinary.js

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,9 +1,9 @@
 import cloudinary from 'cloudinary';
 
 cloudinary.config({
-   cloud_name: 'dzklcbquq',
-   api_key: '855838467369671',
-   api_secret: '3L5W7jfQrQBbrQt_rSZLdVR_oAU'
+   cloud_name: 'cloud_name',
+   api_key: 'api_key',
+   api_secret: 'api_secret'
 });
 
 export default cloudinary;
